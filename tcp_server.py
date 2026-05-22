b590ee05949b4a795fabd65d29036f2cbe017f9f
0x10jalencas <jessalencaster@gmail.com>
2026-05-22 16:14:11 -0700

    working on server

---
+++ tcp_server.py
@@ -0,0 +1,46 @@
+import struct
+import socket
+from dataclasses import dataclass
+
+localhost = "127.0.0.1"
+
+@dataclass
+class Client_Packet:
+    seq_number: int
+    checksum: int
+    data: bytes
+
+@dataclass
+class Server_Packet:
+    ack_number: int
+
+def checksum(data):
+    return sum(data) % 65535
+
+def deserialize_client_packets(packet_bytes):
+    header = packet_bytes[:6]
+    data = packet_bytes[6:]
+
+    seq_number, checksum_value = struct.unpack("!IH", header)
+
+    return Client_Packet(
+        seq_number=seq_number,
+        checksum=checksum_value,
+        data=data
+    )
+
+def main():
+
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(("localhost", 5000))
+    while True:
+        full_packet, client_address = server_socket.recvfrom(1030)
+
+        packet = deserialize_client_packets(full_packet)
+
+        if checksum(packet.data) != packet.checksum:
+            continue
+    
+        ack_number = packet.seq_number + len(packet.data)
+        ack_bytes = struct.packet("!I", ack_number)
