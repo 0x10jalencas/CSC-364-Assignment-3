2e1ce484790ea01cfc033736c0e317f6ff695e91
0x10jalencas <jessalencaster@gmail.com>
2026-05-22 00:20:49 -0700

    add: trying to get packets going

--- tcp_client.py
+++ tcp_client.py
@@ -10,15 +10,7 @@ class Client_Packet:
 class Server_Packet:
     ack_number: int
 
-one_chunk = 1024
 last_ack_number = 0
-
-with open('gistfile1.txt', 'rb') as f:
-    chunk = f.read(one_chunk)
-    if not chunk:
-        break
-    
-
 
 def slow_start(cwnd, ssthresh, ack, rtt):
     for server_packet in ack:
@@ -39,3 +31,45 @@ def congestion_avoidance(cwnd, ack, ssthresh, rtt):
 
     return cwnd, last_ack_number
 
+def fast_retransmit(cwnd, ack, ssthresh, rtt, unacked_packets):
+
+    counts = {}
+    retransmit_packets = set()
+
+    for server_packet in ack:
+        ack_number = server_packet.ack_number
+
+        counts[ack_number] = counts.get(ack_number, 0) + 1
+
+        if counts[ack_number] == 3:
+            if ack_number in unacked_packets:
+                retransmit_packets.add(ack_number)
+
+                ssthresh = max(cwnd / 2, 1)
+                cwnd = ssthresh
+
+    return cwnd, ssthresh, retransmit_packets
+
+def main():
+
+    one_chunk = 1024
+    packets = []
+
+    seq_number = 0
+
+    with open('gistfile1.txt', 'rb') as f:
+        while True:
+            chunk = f.read(one_chunk)
+            if not chunk:
+                break
+
+            packet = Client_Packet(
+                seq_number=seq_number,
+                checksum=0,
+                data=chunk
+            )
+            
+            packets.append(packet)
+            seq_number += len(chunk)
+
+        return packets
